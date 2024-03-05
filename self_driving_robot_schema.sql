1719add79db89a13951d5786f54fa729b6ae9ffe
MuhammedAbdalla <muhabdalla718@gmail.com>
2024-03-04 23:44:43 -0500

    added schema to the tables

--- self_driving_robot_schema.sql
+++ self_driving_robot_schema.sql
@@ -2,31 +2,16 @@
 -- Authentication and Authorization Module
 -- 64 bit size username
 -- MD5 Hash 128 bits
-CREATE TABLE Users (
-    id INT PRIMARY KEY IDENTITY,
-    username VARCHAR (64) NOT NULL,
-    password_hash char (128) NOT NULL,
-    permission INT NOT NULL
-);
+CREATE SCHEMA Sensors;
+GO
 
-CREATE TABLE Logs.AccessLogs (
-    id INT PRIMARY KEY IDENTITY,
-    user_id INTEGER,
-    time_stamp DATETIME2 DEFAULT CURRENT_TIMESTAMP,
-    command TEXT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES Users(id)
-);
+CREATE SCHEMA Car;
+GO
 
--- Wheel Module
-CREATE TABLE Commands.WheelCommands (
-    id INTEGER PRIMARY KEY IDENTITY,
-    time_stamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    speed FLOAT,
-    direction VARCHAR (64),
-    direction_angle FLOAT,
-    duration INT
-);
+CREATE SCHEMA Logs;
+GO
 
+-- SENSORS
 -- LiDAR Module
 CREATE TABLE Sensors.LiDAR (
     id INT PRIMARY KEY IDENTITY,
@@ -41,6 +26,14 @@ CREATE TABLE Sensors.Camera (
     image_data BLOB,
 );
 
+-- CAR
+CREATE TABLE Car.Users (
+    id INT PRIMARY KEY IDENTITY,
+    username VARCHAR (64) NOT NULL,
+    password_hash char (128) NOT NULL,
+    permission INT NOT NULL
+);
+
 -- Visual Object Detection Module
 CREATE TABLE Car.DetectedObjects (
     id INT PRIMARY KEY IDENTITY,
@@ -49,7 +42,7 @@ CREATE TABLE Car.DetectedObjects (
     object_type TEXT,
     confidence FLOAT,
     position FLOAT, 
-    FOREIGN KEY(camera_feed_id) REFERENCES CameraFeeds(id)
+    FOREIGN KEY(camera_feed_id) REFERENCES Sensors.Camera(id)
 );
 
 -- Navigation Module
@@ -60,12 +53,21 @@ CREATE TABLE Car.NavigationRoutes (
     created_at DATETIME2 DEFAULT CURRENT_TIMESTAMP
 );
 
+-- LOGS
 -- Collision Avoidance Detection
 CREATE TABLE Car.CollisionDetection (
     id INT PRIMARY KEY IDENTITY,
     time_stamp DATETIME2 DEFAULT CURRENT_TIMESTAMP,
     severity INT,
     information TEXT
+);
+
+CREATE TABLE Logs.AccessLogs (
+    id INT PRIMARY KEY IDENTITY,
+    user_id INTEGER,
+    time_stamp DATETIME2 DEFAULT CURRENT_TIMESTAMP,
+    command TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES Car.Users(id)
 );
 
 -- Monitoring
@@ -75,4 +77,15 @@ CREATE TABLE Logs.SystemHealth (
     component CHAR (64),
     component_status CHAR (64),
     component_information CHAR (64)
+);
+
+
+-- Wheel Module
+CREATE TABLE Logs.WheelCommands (
+    id INTEGER PRIMARY KEY IDENTITY,
+    time_stamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    speed FLOAT,
+    direction VARCHAR (64),
+    direction_angle FLOAT,
+    duration INT
 );
